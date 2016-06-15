e2fc20c6b3e74883d89fc249633b45f029fdbc63
hwgao <hhww.gao@gmail.com>
2016-06-15 20:28:43 +0800

    add Vundle

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -20,7 +20,6 @@ echo "done"
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
@@ -29,3 +28,10 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo -n "Clone Vundle ..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo -n "Install all vim plugins ..."
+vim +PluginInstall +qall
+echo "done"
