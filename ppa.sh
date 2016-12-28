8c9c1a3f09db7b471a6a1d97e8bcbebf0b7a7c35
hongwei.gao <hongwei.gao@mstarsemi.com>
2016-12-28 22:58:32 +0800

    rm tig and update ag

--- ppa.sh
+++ ppa.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+# Test no ubuntu 12.04 32bit server
+
+#sudo apt-get update
+#sudo apt-get upgrade
+sudo apt-get install python-software-properties
 
 # VIM
 sudo add-apt-repository ppa:fcwu-tw/ppa
 #TMUX
 sudo add-apt-repository ppa:pi-rho/dev
 #ag
-sudo add-apt-repository ppa:pgolm/the-silver-searcher
-#tig
-sudo add-apt-repository ppa:clock/tig
+sudo add-apt-repository ppa:gekkio/ag
+#git
+sudo add-apt-repository ppa:git-core/ppa
 
 sudo apt-get update
 
 sudo apt-get install vim
 sudo apt-get install tmux
 sudo apt-get install the-silver-searcher
-sudo apt-get install tig
+sudo apt-get install git
