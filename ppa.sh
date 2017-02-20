9a29e06d2229ecb7b46366710ad4514645ac5b05
hongwei.gao <hongwei.gao@mstarsemi.com>
2017-02-20 14:43:06 +0800

    add gcc

--- ppa.sh
+++ ppa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Test no ubuntu 12.04 32bit server
+# Test on ubuntu 12.04 32bit server
 
 #sudo apt-get update
 #sudo apt-get upgrade
@@ -20,3 +20,11 @@ sudo apt-get install vim
 sudo apt-get install tmux
 sudo apt-get install the-silver-searcher
 sudo apt-get install git
+
+# Install gcc 4.9
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 50
+sudo apt-get install g++-4.9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
