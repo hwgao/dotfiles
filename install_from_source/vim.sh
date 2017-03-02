70c11cd429ca23b655399b1d96e0b42ebb265ebd
hongwei.gao <hongwei.gao@mstarsemi.com>
2017-03-02 14:33:28 +0800

    correct python issue

--- install_from_source/vim.sh
+++ install_from_source/vim.sh
@@ -15,21 +15,24 @@ sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
 cd ${SRC_ROOT}
 git clone https://github.com/vim/vim.git
 cd vim
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
-            --enable-perlinterp=yes \
-            --enable-luainterp=yes \
-            --enable-gui=gnome2 \
-            --enable-cscope --prefix=/usr
+./configure \
+    --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp=yes \
+    --enable-pythoninterp=yes \
+    --with-python-config-dir=$(/usr/bin/python2-config  --configdir) \
+    --enable-python3interp=yes \
+    --with-python3-config-dir=$(/usr/bin/python3-config --configdir) \
+    --enable-perlinterp=yes \
+    --enable-luainterp=yes \
+    --enable-cscope \
+    --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
 
-sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox
-sudo make install
+sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox vim-runtime
+#sudo make install
+sudo checkinstall
+#dpkg -r vim # to remove
 
 #Set vim as your default editor with update-alternatives.
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
