9307ae3b57af15eb085844974b4b9c09c4301b5a
hongwei.gao <hongwei.gao@mstarsemi.com>
2017-08-16 17:28:58 +0800

    use cargo to install ripgrep

--- install_from_source/ripgrep.sh
+++ install_from_source/ripgrep.sh
@@ -4,9 +4,13 @@ if [ ! -f ~/.cargo/env ]; then
     # Install rust
     curl https://sh.rustup.rs -sSf | sh
     source ~/.cargo/env
+else
+    rustup self update
+    rustup update
 fi
 
-. ./git-lastest-tag https://github.com/BurntSushi/ripgrep ~/src_root -i
-cargo build --release
-cp target/release/rg ~/bin/
+cargo install ripgrep
+# . ./git-lastest-tag https://github.com/BurntSushi/ripgrep ~/src_root -i
+# cargo build --release
+# cp target/release/rg ~/bin/
 rg --version
